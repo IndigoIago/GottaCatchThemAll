e34c977ff7046cdaec0dddb8c3c675a703b4be19
Jim OBrien <jimobrien930@gmail.com>
2014-08-29 15:34:00 -0700

    swapped ngRoute for ui.router

--- client/app/app.js
+++ client/app/app.js
@@ -2,5 +2,6 @@ angular.module('catchem', [
   'catchem.auth',
   'catchem.game',
   'catchem.user',
-  'catchem.services'
+  'catchem.services',
+  'ui.router'
 ]); // this will remain barebones; it only loads modules.
