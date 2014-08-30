4b896bf0f0c3d6775b9dbae96a1dd502241df131
SterlingVix <amelocik@gmail.com>
2014-08-29 19:12:18 -0700

    Working basic server up in 2 parts: Server and Router. See comments.

--- server/router.js
+++ server/router.js
@@ -2,31 +2,11 @@
  * Router for Express Server
  * HTTP verbs like 'GET', 'POST', etc. are defined here.
  * Implemented with Express - http://expressjs.com/4x/api.html#router
+ * 
  ********************/
-// var express = require('express'); NECESSARY?!?!?!?
+var defaultMessage = 'NOTE: Inside route.';
 
-var router = express.Router(); // Express router
-
-var defaultMessage = 'NOTE: Inside app.route.';
-
-/* ///   TEST   ///
-router.get('/', function(req, res) {
-	res.json({ message: 'hooray! welcome to our api!' });	
-});
-*/
-
-
-
-
-app.route('/') // route to handle all HTTP verbs at '/'
-  .get(function(req, res){ // listen for 'GET' request, and send data
-    console.log(defaultMessage + 'GET');
-    res.send(defaultMessage + 'GET');
-  // ALT: res.json({ message: defaultMessage });	
-});
-
-// app.route('/') //  TEMP! Keeping this until verifying route() works
-//    .get('/', function(req, res){ // listen for this get ( '/' ) request, and send data
-//   console.log(defaultMessage);
-//   res.send(defaultMessage);
-// });
+exports.index = function(req, res){
+  console.log(defaultMessage + 'GET');
+  res.json({ message: defaultMessage + 'GET' }); // NOTE: alt syntax ==> res.end(defaultMessage + 'GET');
+}; // end index
--- server/server.js
+++ server/server.js
@@ -1,17 +1,18 @@
 /*****************
  * Basic Server
  * Implemented with Express - http://expressjs.com/4x/api.html
+ * Run with nodemon by running 'nodemon server.js' from this folder.
  ****************/
-var router = require('./router.js');
 var express = require('express');
+var app = express();
+var router = require('./router');
 var bodyParser = require('body-parser'); // to get data from POST: https://github.com/expressjs/body-parser
 
-var app = express();
 var localPort = process.env.PORT || 3003; // hard-coded port
 
-/***********
+/*****************
  * Headers
- ***********/
+ ****************/
 app.all('*', function(req, res, next) { // headers for server
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
@@ -19,12 +20,13 @@ app.all('*', function(req, res, next) { // headers for server
 });
 
 /*****************
- * NOTE: This code block included for review. I recommend we keep until we deploy, then
- * remove or un-comment as necessary.
+ * NOTE: This code block included for review. I recommend we review it before
+ * we deploy, then remove or un-comment as necessary.
        app.set('title', 'CatchEm'); // set site title in server.
        app.get('title');
  ****************/
-  
+
+
 /*****************
  * NOTE: This code block included for later implementation.
  * Use bodyParser to parse application/json as follows:
@@ -32,5 +34,15 @@ app.all('*', function(req, res, next) { // headers for server
        app.use(bodyParser.json()) 
  ****************/
 
+
+/*****************
+ * Routes
+ ****************/
+app.get('/', router.index); // call router.index on get '/'
+
+
+/*****************
+ * Start Server
+ ****************/
 app.listen(localPort); // Start the serverconsole.log('Magic happens on port ' + port);
 console.log('Listening on port ' + localPort); // Log to console on successful start
