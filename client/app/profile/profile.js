5506e979575345401209d446570c789397c74022
Jim OBrien <jimobrien930@gmail.com>
2014-09-01 15:36:34 -0700

    began profile interface logic

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -9,7 +9,17 @@ angular.module('catchem.profile', ['catchem.services']) // Load the service modu
     templateUrl: 'app/profile/profile.html',
     replace: true,
     link: function (scope, iElement, iAttrs) {
-      
+      scope.currentQuestion = scope.data.questions[0];
+
+      scope.submitAnswer = function (answer) {
+        var result = answer === scope.currentQuestion.a;
+        
+        // scope.$emit('roundFinshed', result);
+
+        // var remainingQuestions = scope.data.questions.splice(1);
+        // if ( remainingQuestions.length )
+        //   play(remainingQuestions);
+      };
     }
   };
 }]);
