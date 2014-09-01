b57da53ccffef417344a3608a6c2d137211fa593
Jim OBrien <jimobrien930@gmail.com>
2014-09-01 12:54:17 -0700

    (feat) basic html structure for views

--- client/app/game/game.js
+++ client/app/game/game.js
@@ -2,4 +2,13 @@
 
 //TODO: Determine if game logic goes here
 
-angular.module('catchem.game', ['catchem.services']); // Load the service module as a dependancy
+angular.module('catchem.game', ['catchem.services'])// Load the service module as a dependancy
+.controller('GameCtrl', ['$scope', function ($scope) {
+  $scope.aProfile = {
+    name: {
+      first: 'Douglas',
+      last: 'Crockford'
+    }
+  };
+  
+}]); 
