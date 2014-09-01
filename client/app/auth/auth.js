655382f9cc38b92f22eadd027179aedb455b440d
Jim OBrien <jimobrien930@gmail.com>
2014-09-01 12:38:49 -0700

    removed extra app.js file. fixed src paths in index.

--- client/app/auth/auth.js
+++ client/app/auth/auth.js
@@ -1,14 +1,3 @@
 // Authorization module for persistent user access
 // Load the service module as a dependancy
-angular.module('catchem.auth', ['catchem.services'])
-.config(function ($stateProvider, $urlRouterProvider, $httpProvider) {
-  $stateProvider
-    .state('login', {
-      url: '/login',
-      // templateUrl: 'app/auth/login.html',
-      // controller: 'AuthController'
-    });
-
-  $urlRouterProvider.otherwise('/login');
-
-});
+angular.module('catchem.auth', ['catchem.services']);
