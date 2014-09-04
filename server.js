3cc39a1b62006fac9e0369df4ddaff7a8c28adb8
iagowp <iago.wp@hotmail.com>
2014-09-04 15:41:56 -0700

    (config) autodeploy to azure working

--- server.js
+++ server.js
@@ -3,7 +3,7 @@
  * Implemented with Express - http://expressjs.com/4x/api.html
  * Run with nodemon by running 'nodemon server.js' from this folder.
  ****************/
-var app = require('./router');
+var app = require('./server/router');
 
 var localPort = process.env.PORT || 3003; // hard-coded port
 
@@ -12,7 +12,6 @@ var localPort = process.env.PORT || 3003; // hard-coded port
  ****************/
 app.listen(localPort); // Start the serverconsole.log('Magic happens on port ' + port);
 console.log('Listening on port ' + localPort); // Log to console on successful start
-
 
 
 
