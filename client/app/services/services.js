50b1ea0573ab3ac1930b4cec56c5a29735f52193
Jim OBrien <jimobrien930@gmail.com>
2014-09-04 02:03:27 -0700

    (feat) created placeholder user service + get/add methods

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,5 +1,20 @@
 // Services common across many modules
 angular.module('catchem.services', [])
+
+// Temp/placeholder User service.
+.service('User', ['$rootScope', function ($rootScope) {
+  var self = this;
+  var profileCollection = [];
+
+  self.addProfileToCollection = function (profile) {
+    profileCollection.push(profile);
+  };
+
+  self.getProfileCollection = function () {
+    return profileCollection;
+  }
+}])
+
 // Collection service for adding / retrieving a users' collection
 .service('Collection', ['$rootScope', 'User', function ($rootScope, User) {
   var self = this;
