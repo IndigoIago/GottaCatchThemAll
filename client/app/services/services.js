5ed4bff9e84f8a27ca096d323e07b4c7e90512fb
Jim OBrien <jimobrien930@gmail.com>
2014-09-04 02:00:40 -0700

    (feat) add collection service

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,2 +1,15 @@
 // Services common across many modules
-angular.module('catchem.services', []);
+angular.module('catchem.services', [])
+// Collection service for adding / retrieving a users' collection
+.service('Collection', ['$rootScope', 'User', function ($rootScope, User) {
+  var self = this;
+  var profileCollection = User.getProfileCollection();
+
+  self.addProfile = function (profile) {
+    User.addProfileToCollection(profile);
+  };
+
+  self.getCollection = function () {
+    return profileCollection;
+  };
+}]);
