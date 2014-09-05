be5fed6191170739808c0285e62a3b95beccd6c7
Jim OBrien <jimobrien930@gmail.com>
2014-09-05 10:23:56 -0700

    (cleanup) remove debugging object

--- client/app/collection/collection.js
+++ client/app/collection/collection.js
@@ -1,18 +1,5 @@
 // Collection module - contains related modules for displaying the collection view
 angular.module('catchem.collection', ['catchem.services', 'collection.people'])
 .controller('CollectionCtrl', ['$scope', 'Collection', function ($scope, Collection) {
-  // $scope.peopleCollected = Collection.getCollection();
-  $scope.peopleCollected = [{
-      name: 'Douglas Crockford',
-      photo: './img/douglas_c.png',
-      questions: [
-        { question: 'Do I like JavaScript?',
-          answer: true
-        },
-        { question: 'I like incrementing numbers with "++"',
-          answer: false
-        }
-      ],
-      pointValue: 1400
-    }]
+  $scope.peopleCollected = Collection.getCollection();
 }]);
