05219d8bc66b89102951853212ff2c2f57af35db
SterlingVix <amelocik@gmail.com>
2014-09-05 19:05:07 -0700

    Add feedback and modularity to game, and change views.

--- client/app/user/user.js
+++ client/app/user/user.js
@@ -15,3 +15,34 @@ angular.module('catchem.user', ['catchem.services'])
     pointValue: 1400
   };
 }]);
+
+
+
+/*          MOSTLY-OFFICIAL USER TEMPLATE
+  id = {
+    id: 1234561,
+    full_name:    'Douglas Crockford',
+    first_name:   'Douglas',
+    last_name:    'Crockford',
+    email:        'd@g.com',
+    gender:       'Male'
+    photo:        './img/douglas_c.png',
+    about:        'I invented JavaScript. Bow down to me.',
+    questions: [
+      { question: 'Do I like JavaScript?', answer: true},
+      { question: 'I like incrementing numbers with "++"', answer: false},
+      { question: 'Do I like incrementing numbers with "++"?', answer: false}
+    ],
+    pointValue:   0,
+    visitedProfiles: {
+      lastVisited: 1/1/14, // can make array
+      status: 'collected', // or 'rejected' or 25%
+    },
+    collection: {
+      // id: id
+    },
+    owners: {
+      // id: id
+    }
+  };
+*/
