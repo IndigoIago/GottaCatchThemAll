5ca5669eedf83373e4d370b86348f4c20437be53
Jim OBrien <jimobrien930@gmail.com>
2014-09-07 11:15:27 -0700

    (cleanup) comments

--- client/app/game/profile/profile.js
+++ client/app/game/profile/profile.js
@@ -2,6 +2,7 @@
 angular.module('game.profile', []) // Load the service module as a dependancy
 .directive('profile', ['$timeout', function ($timeout) {
 
+  // helper to set progress bar percentage
   var increaseProgressBar = function (element, questionCount, questionsRemaining) {
     var percentage = ((questionCount - questionsRemaining) / questionCount) * 100 + '%';
     
@@ -10,6 +11,7 @@ angular.module('game.profile', []) // Load the service module as a dependancy
     });
   };
 
+  // helper to blur photos
   var setPhotoBlur = function (photo, amount) {
     photo.css({
       '-webkit-filter': 'blur(' + amount + 'px)',
@@ -31,8 +33,11 @@ angular.module('game.profile', []) // Load the service module as a dependancy
       var progressEl = angular.element(document.querySelector('.progress-bar'));
       var photo = angular.element(document.querySelector('.photo')).find('img');
 
+      // get first question from questions list
       scope.currentQuestion = questions.shift();
 
+      // set initial photo blur
+      // Formula = 4 (arbitrary base blur amount) multiplied by the amount of questions
       setPhotoBlur(photo, (4 * questionCount));
 
       scope.submitAnswer = function (answer) {
@@ -40,30 +45,39 @@ angular.module('game.profile', []) // Load the service module as a dependancy
 
         if ( isCorrect ) {
           if ( !questions.length ) {
+            // set winning text
             questionEl.text('You captured ' + scope.data.name + '!');
+
+            // invoke round handler with result
             scope.roundHandler({ winner: true });
           }
 
+          // increase progress bar
           increaseProgressBar(progressEl, questionCount, questions.length);
+
+          // update photo blur
           setPhotoBlur(photo, (4 * questions.length));
 
+          // set next question
           scope.currentQuestion = questions.shift();
 
         } else {  
+          // make progress bar red
           progressEl.css({
             transition: 'none',
             width: '100%',
             background: '#d9534f'
           });
+
+          // set loosing text
           questionEl.text(scope.data.name + ' has escaped :(');
+
+          // invoke round handler with result
           scope.roundHandler({ winner: false} ); 
         }
       }; 
 
     }
   };
-
-
-
 
 }]);
