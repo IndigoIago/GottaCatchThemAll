a2a4badcd07227e10266de674d3afb1af26685c9
SterlingVix <amelocik@gmail.com>
2014-09-08 11:54:59 -0700

    Inital merge conflict work

--- client/app/services/services.js
+++ client/app/services/services.js
@@ -1,7 +1,7 @@
 // Services common across many modules
 angular.module('catchem.services', [])
 
-// Temp/placeholder User service.
+// User service for manipulating the current player's data
 .service('User', ['$rootScope', '$http', function ($rootScope, $http) {
 
   /***********
@@ -16,10 +16,7 @@ angular.module('catchem.services', [])
   /***********
    * Player properties accessible across many scopes
    **********/
-
   var self = this;
-  var profileCollection = [];
-
   var player = {}; // logged in player's profile, PRIVATE in this scope
 
 
@@ -67,92 +64,89 @@ angular.module('catchem.services', [])
     return player;
   }; // end getPersonalProfile()
 
-  // // Logged in user's profile
-  // var personalProfile = {};
   self.setPersonalProfile = function(key, val) { // Setter for logged in player's profile
     player[key] = val;
     self.savePlayerProfileToDB(); // UPDATE db
   }; // end setPersonalProfile(key, val)
 
-
-  // self.addProfileToCollection = function (profile) {
-  //   profileCollection.push(profile);
-  // };
   self.getProfileCollection = function () { // Getter for logged in player's collection
   console.log('player = ', player);
     return player.collection;
   }; // end getProfileCollection()
 
-+  self.addProfileToCollection = function (capturedProfile) { // Setter to add to logged in player's collection
-+    var capID = capturedProfile.id;
-+    player.collection.capID = capID; // add this ID to the collection object
-+    self.savePlayerProfileToDB(); // UPDATE db
-+  }; // end addProfileToCollection(capturedProfile)
+  self.addProfileToCollection = function (capturedProfile) { // Setter to add to logged in player's collection
+    var capID = capturedProfile.id;
+    player.collection.capID = capID; // add this ID to the collection object
+    self.savePlayerProfileToDB(); // UPDATE db
+  }; // end addProfileToCollection(capturedProfile)
 
-
-
-
-
-
-
-
-
-
-
-  // Getter and Setter for Logged in user's profile
-  self.getPersonalProfile = function() {
-    return personalProfile;
-  };
-  self.setPersonalProfile = function(key, val) {
-    personalProfile[key] = val;
-  };
-
-  // Get the profile from DB
-  self.retreivePersonalProfileFromDB = function() {
+  self.getProfilesFromDB = function (numOfProfilesToGet) { // Getter for a bunch of profiles from DB
+    var urlString = '/query/profiles?';
+    if (numOfProfilesToGet) urlString += numOfProfilesToGet;
     $http({
-      url: '/userprofile',
+      url: urlString, // '/query/profiles?20',
       method: 'GET'
-    })
-    .then(function(response) {
-      // Success
-      personalProfile = response.data;
-      console.log("Got profile!", response.data);
-    }, 
-    function(response) { // optional
-      // Error
-      console.log("Error retrieving user profile");
-    });
-  };
-  self.retreivePersonalProfileFromDB();
+    }) // end http()
+    .then(function(response) { // Success
+      console.log("Got profiles:", response.data);
+      return response.data;
+    }, // end (success)
+    function(response) { // optional error handling
+      console.log("Error retrieving profiles", response.data);
+    }); // end then()
+  }; // end getProfilesFromDB()
 
-  // Save the profile to DB
-  self.savePersonalProfileToDB = function() {
+  /*************     NOTE: Not yet functional!     ****************
+  self.getUserByID = function(userID) { // TODO - refactor this so ID is optional?!?!?!?
+    var urlString = '/query/id?' + userID;
     $http({
-      url: '/userprofile',
-      method: 'POST',
-      data: self.getPersonalProfile()
-    })
-    .then(function(response) {
-      // Success
-      console.log("Saved user profile.");
-    }, 
-    function(response) { // optional
-      // Error
-      console.log("Error saving user profile.");
-    });
-  };
-}])
+      url: urlString,
+      method: 'GET'
+    }) // end http GET request passing JSON with id
+    .then(function(response) { // Success
+      console.log("Got profile!", response.data);
+      return response.data; // return the user with this id
+    }, // end (success)
+    function(response) { // optional error handling
+      console.log("Error retrieving player profile", response.data);
+    }); // end then()
+  }; // end getUserByID(userID)
 
-// Collection service for adding / retrieving a users' collection
+  self.updateUserByID = function(userID, jsonObjToUpdate) { // TODO - refactor this so ID is optional?!?!?!?
+    var urlString = '/query/id?' + userID;
+    $http({
+      url: urlString,
+      method: 'POST',
+      data: jsonObjToUpdate
+    }) // end http POST request passing JSON with id
+    .then(function(response) { // Success
+      console.log("Got profile!", response.data);
+      return true; // not really needed except for possible error handling
+    }, // end (success)
+    function(response) { // optional error handling
+      console.log("Error saving player profile.", response.data);
+      return false; // not really needed except for possible error handling
+    }); // end then()
+  }; // end getUserByID(userID)
+  *****************************************************/
+}]) // end .service(User)
+
+// TODO: REMOVE this block as per discussion with Jim and refactor elsewhere --- Aaron
+// Collection service for adding / retrieving a players' collection
 .service('Collection', ['$rootScope', 'User', function ($rootScope, User) {
   var self = this;
   var profileCollection = User.getProfileCollection();
 
-  self.addProfile = function (profile) {
-    User.addProfileToCollection(profile);
-  };
+  self.addProfile = function (capturedProfile) {
+    User.addProfileToCollection(capturedProfile);
+  }; // end addProfile(profile)
 
-  self.getCollection = function () {
-    return profileCollection;
-  };
-}]);
+  self.getCollection = function (playerProfile) {
+    
+    return User.getProfileCollection(profile);
+  }; // end getCollection()
+
+  self.getDBProfiles = function(numOfProfiles) {
+    return User.getProfilesFromDB(numOfProfiles);
+  }
+}]); // end .service(Collection)
--- client/app/user/user.js
+++ client/app/user/user.js
@@ -1,12 +1,11 @@
 angular.module('catchem.user', ['catchem.services'])
 .factory("UserFactory", function(User){
   var myProfile = User.getPersonalProfile();
-
   var updateAbout = function(message) {
     User.setPersonalProfile("about", message);
     User.savePersonalProfileToDB();
     myProfile = User.getPersonalProfile();
-  };
+  }; // end updateAbout(message)
 
   var addQuestion = function(question, answer) {
     if (!myProfile.questions) {
@@ -21,7 +20,7 @@ angular.module('catchem.user', ['catchem.services'])
     User.setPersonalProfile("questions", myProfile.questions);
 
     User.savePersonalProfileToDB();
-  }
+  }; // end addQuestion(question, answer)
 
   return {
     updateAbout: updateAbout,
@@ -47,35 +46,41 @@ angular.module('catchem.user', ['catchem.services'])
       UserFactory.addQuestion($scope.question, $scope.answer);
     }
   }
-}]);
 
 
 
-/*          MOSTLY-OFFICIAL USER TEMPLATE
-  id = {
-    facebook_id: 1234561,
-    full_name:    'Douglas Crockford',
-    first_name:   'Douglas',
-    last_name:    'Crockford',
-    email:        'd@g.com',
-    gender:       'Male'
-    photo:        './img/douglas_c.png',
-    about:        'I invented JavaScript. Bow down to me.',
-    questions: [
-      { question: 'Do I like JavaScript?', answer: true},
-      { question: 'I like incrementing numbers with "++"', answer: false},
-      { question: 'Do I like incrementing numbers with "++"?', answer: false}
-    ],
-    pointValue:   0,
-    visitedProfiles: {
-      lastVisited: 1/1/14, // can make array
-      status: 'collected', // or 'rejected' or 25%
-    },
-    collection: {
-      // id: id
-    },
-    owners: {
-      // id: id
-    }
-  };
-*/
+  $scope.makeConformantProfileObject = function(user) {
+    if (user.pointValue > 0) { // this is already conformant
+      return user;
+    } // end if (is already conformant)
+
+    var thisUser = getNewUserTemplate(); // get an empty user/profile template object
+
+    thisUser.id         = user.id;
+    thisUser.full_name  = user.full_name
+    thisUser.first_name = user.first_name
+    thisUser.last_name  = user.last_name
+    thisUser.email      = user.email
+    thisUser.gender     = user.gender
+    if (user.photo) {
+      thisUser.photo    = user.photo; // use existing photo, which must have been granted by FB API?
+    } else { // generate avatar for player
+      thisUser.photo    = './img/user_icon' + thisUser.gender.charAt(0) + (id%5) + '.png'; // generate random avatar
+    } // end if (photo)
+    thisUser.about      = 'I\'m brand new here. But I\'m here, so I have great taste!';
+    thisUser.pointValue = 1; // first one's free. :-D
+    // thisUser.questions  =      // Not needed, empty object is fine.
+    // thisUser.collection =      // Not needed, empty object is fine.
+    // thisUser.owners     =      // Not needed, empty object is fine.
+    // thisUser.visitedProfiles = // Not needed, empty object is fine.
+
+    return thisUser; // return the conformant profile object
+  }; // end makeConformantProfileObject(user)
+
+
+
+
+
+
+
+}]); // end UserController controller
